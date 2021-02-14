918d2c98284b8a383357001f8c895867c3b2488b
Andrew Marks <andrewmarks@Andrews-Mac-Mini.local>
2021-02-14 20:07:41 +0000

    Updates

---
+++ precache-manifest.c99c7bf352a70b8f62902946b69b9da8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1357aee677ec9b4426f877c20d614930",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "f1c8d0cdc8986b11c02a",
+    "url": "/ToDoApp/static/js/2.0ea2811c.chunk.js"
+  },
+  {
+    "revision": "5430e0e5b45eab24dce0",
+    "url": "/ToDoApp/static/js/main.028f02d3.chunk.js"
+  },
+  {
+    "revision": "3ab383f6d7c176f480a2",
+    "url": "/ToDoApp/static/js/runtime-main.adb1fbed.js"
+  }
+]);
